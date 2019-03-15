36ae62f6459434e38bdd9631323f304d091b73a8
Ben Kissi <benkissi111@gmail.com>
2019-03-15 13:33:17 +0000

    added error handler

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ const port = process.env.PORT || 3000;
 
 app.use(cors())
 
-app.post('/users', async (req, res) => {
+app.post('/users', async (req, res, next) => {
     try{
         const body = _.pick(req.body, ['email', 'name', 'authToken', 'id']);
         
@@ -55,56 +55,86 @@ app.post('/users', async (req, res) => {
         });
         
     }catch(e) {
-        res.status(400).send(e);
-        console.log(e)
+        next(e)
     }
     
 });
 
-app.get('/adaccounts', authenticate, async (req,res) => {
-    const id = req.user.userId;
-    const list = await FB.api(`/${id}/adaccounts?fields=name,account_status`, {
+app.get('/adaccounts', authenticate, async (req,res, next) => {
+    try{
+        const id = req.user.userId;
+        const list = await FB.api(`/${id}/adaccounts?fields=name,account_status`, {
         access_token: req.user.authToken
-    });
-    res.send(list);
+        });
+        res.send(list);
+    }catch(e) {
+        next(e)
+    }
 });
 
-app.get('/campaigns/:id', authenticate, async (req,res) => {
-    const id = req.params.id;
-    const list = await FB.api(`/${id}/campaigns?fields=name&effective_status[]=ACTIVE`, {
+app.get('/campaigns/:id', authenticate, async (req,res, next) => {
+    try{
+        const id = req.params.id;
+        const list = await FB.api(`/${id}/campaigns?fields=name&effective_status[]=ACTIVE`, {
         access_token: req.user.authToken
-    });
-    res.send(list);
+        });
+        res.send(list);
+    }catch(e) {
+        next(e)
+    }
 });
 
-app.get('/adsets/:id', authenticate, async (req,res) => {
-    const id = req.params.id;
-    const list = await FB.api(`/${id}/adsets?fields=name`, {
-        access_token: req.user.authToken
-    });
-    res.send(list);
+app.get('/adsets/:id', authenticate, async (req,res, next) => {
+    try {
+        const id = req.params.id;
+        const list = await FB.api(`/${id}/adsets?fields=name`, {
+            access_token: req.user.authToken
+        });
+        res.send(list);
+    }catch(e) {
+        next(e)
+    }
 });
 
-app.get('/ads/:id', authenticate, async (req,res) => {
-    const id = req.params.id;
-    const list = await FB.api(`/${id}/ads?fields=name`, {
+app.get('/ads/:id', authenticate, async (req,res,next) => {
+    try{
+        const id = req.params.id;
+        const list = await FB.api(`/${id}/ads?fields=name`, {
         access_token: req.user.authToken
-    });
-    res.send(list);
+        });
+        res.send(list);
+    }catch(e) {
+        next(e)
+    }
 });
 
-app.get('/stats/:id', authenticate, async (req,res) => {
-    const id = req.params.id;
-    const stats = await FB.api(`/${id}/keywordstats?fields=impressions,clicks,cpc,ctr,spend,reach`, {
+app.get('/stats/:id', authenticate, async (req,res, next) => {
+    try{
+        const id = req.params.id;
+        const stats = await FB.api(`/${id}/keywordstats?fields=impressions,clicks,cpc,ctr,spend,reach`, {
         access_token: req.user.authToken
-    });
-    res.send(stats);
-});
-
-app.get('/up', (req,res) => {
+        });
+        res.send(stats);
+    }catch(e) {
+        next(e)
+    }
     
+    
+});
+
+app.get('/up', (req,res,next) => {
     res.send('App is up');
 });
+
+app.use((err, req, res, next) => {
+    if(err){
+        console.log(err)
+        res.status(400).send(err);
+    }
+
+})
+
+
 
 app.listen(port, ()=> {
     console.log(`Server started on port ${port}`);
